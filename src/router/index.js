991e0fd20306384e3766d94d1163769846a44183
Chen Hai Bin <754032348@qq.com>
2020-10-10 09:15:22 +0800

    second

--- src/router/index.js
+++ src/router/index.js
@@ -5,43 +5,17 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    component: () => import('../views/Home.vue'),
-    children: [
-      {
-        path: '/',
-        redirect: '/index'
-      },
-      {
-        path: '/index',
-        component: () => import('../views/Index.vue')
-      },
-      {
-        path: '/classify',
-        component: () => import('../views/Classify.vue')
-      },
-      {
-        path: '/cart',
-        component: () => import('../views/Cart.vue')
-      },
-      {
-        path: '/community',
-        component: () => import('../views/Community.vue')
-      },
-      {
-        path: '/mine',
-        component: () => import('../views/Mine.vue')
-      },
-      {
-        path: '/plan',
-        component: () => import('../views/Plan.vue')
-      },
-      {
-        path: '/homepage',
-        component: () => import('../views/Homepage.vue')
-      },
-    ]
-  }
+    path: '/mine',
+    component: () => import('../views/Mine.vue')
+  },
+  {
+    path: '/plan',
+    component: () => import('../views/Plan.vue')
+  },
+  {
+    path: '/homepage',
+    component: () => import('../views/Homepage.vue')
+  },
 ]
 
 const router = new VueRouter({
