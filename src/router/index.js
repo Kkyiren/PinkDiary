2161dd26e19d65852f87054332831937dabe7f82
jjlhang <1831665770@qq.com>
2020-10-10 11:56:45 +0800

    金首次提交

--- src/router/index.js
+++ src/router/index.js
@@ -78,6 +78,47 @@ const routes = [
     component: () => import("../views/HotNovel.vue")
   },
   {
+    path: '/shopping',
+    component: () =>
+      import('../views/shopping.vue'),
+    children: [
+      {
+        path: '/shopping',
+        redirect: '/choose'
+      },
+      {
+        path: '/choose',
+        component: () =>
+          import('../components/jin/choose.vue')
+      },
+      {
+        path: '/Electrical',
+        component: () =>
+          import('../components/jin/Electrical.vue')
+      },
+      {
+        path: '/phone',
+        component: () =>
+          import('../components/jin/phone.vue')
+      },
+      {
+        path: '/close',
+        component: () =>
+          import('../components/jin/close.vue')
+      },
+      {
+        path: '/Computer',
+        component: () =>
+          import('../components/jin/Computer.vue')
+      },
+      {
+        path: '/beauty',
+        component: () =>
+          import('../components/jin/beauty.vue')
+      }
+    ]
+  },
+  {
     path: "*",
     component: () => import("../views/NotFound.vue")
   }
