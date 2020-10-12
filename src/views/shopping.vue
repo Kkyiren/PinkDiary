e2da6b0b1b163477ffe8500691c2e1f8c91d0510
Kyiren <yirenyishan@gmail.com>
2020-10-12 17:49:49 +0800

    xtest

--- src/views/shopping.vue
+++ src/views/shopping.vue
@@ -1,21 +1,19 @@
 <template>
-    <div id="app">
-            <q-header/>
-            <tabs/>
-            <router-view></router-view>
-    </div>
+  <div id="app">
+    <q-header />
+    <tabs />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import qHeader from '@/components/jin/Header';
-import Tabs from '@/components/jin/Tab';
+import qHeader from "@/components/jin/Header";
+import Tabs from "@/components/jin/Tab";
 // import foter from '@/public/foter'
 export default {
-    components:{
-        qHeader,
-        Tabs,
-    }
-}
+  components: {
+    qHeader,
+    Tabs
+  }
+};
 </script>
-<style lang="scss" scoped>
-</style>
